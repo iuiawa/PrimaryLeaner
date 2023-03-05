49abe2ec6d52bc5e71f7a84b5b1ea021c77414a1
iuiawa <103098292+iuiawa@users.noreply.github.com>
2023-03-05 11:58:04 +0800

    Add files via upload

--- 学习.cpp
+++ 学习.cpp
@@ -11,28 +11,50 @@ IMAGE 图片;
 
 IMAGE Img_role[2];
 IMAGE Img_bullet[2];
+IMAGE ENEMI_A[2][2];
+
+int GameTicks = 0;
+int LastBullTick = 0;
 
 //防止以后的变量冲突，加上自定义类型
 enum Window_Settings
 {
     WIDTH = 640,//窗口宽度
-    HEIGHT = 480,//窗口高度
+    HEIGHT = 640,//窗口高度
     MAX_PLAYER_LEFTH = -30,//最大玩家可达到的左侧位置x
     MAX_PLAYER_HIGTH = -30,//最大玩家可达到的高度y
     MAX_PLAYER_Y_DOWN = HEIGHT - 30,//最大玩家可到达的最低点
     MAX_PLAYER_X_RIGHT = WIDTH - 30,//最大玩家可达到的最右边
     max_bullet_num = 114,//最大子弹数目
+    Bullet_Speed = 15,//子弹飞行速度
+
+    MAX_ENEMI_NUM = 50,//最大敌人数目
+    BIG_PLANE = 1,//大飞机状态TYPE值，不用修改
+    SMALL_PLANE = 0,//小飞机状态TYPE值
+    BIG_PLANE_DEFAULT_HP = 100,//大飞机默认HP
+    SMALL_PLANE_DEFAULT_HP = 10,//小飞机默认HP
+    BIG_PLANE_IMAGE_WIDTH=50,//大飞机碰撞箱与图像宽度
+    SMALL_PLANE_IMAGE_WIDTH=25,//小飞机碰撞箱与图像宽度
+    BIG_PLANE_IMAGE_HEIGHT = 50,//大飞机碰撞箱与图像高度
+    SMALL_PLANE_IMAGE_HEIGHT = 25,//小飞机碰撞箱与图像高度
 };
 
-//玩家的飞只因与子弹结构体
-struct Player_Plane
+//实体数据结构体
+struct Enities
 {
     int x = 0;//玩家的x坐标
     int y=0;//玩家的y坐标
     bool alive=true;//玩家是死是活
     int speed_PX = 5;
+    int health = 114;
+    int Width = 50;//宽度（碰撞箱px）
+    int Height = 50;//高度（碰撞箱px）
+    int type = 0;//类型
 
-}player,bull[max_bullet_num];
+};
+Enities player;
+Enities bull[max_bullet_num];
+Enities enemies[MAX_ENEMI_NUM];
 
 struct Keys
 {
@@ -58,16 +80,42 @@ void Startup_LoadImage() {
     loadimage(&Img_bullet[0], "images\\bullet_a.png");
     loadimage(&Img_bullet[1], "images\\bullet_b.jpg");
 
+
+    //加载敌机图片
+    loadimage(&ENEMI_A[0][0], "images\\Flight_Chicken.jpg");
+    loadimage(&ENEMI_A[0][1], "images\\Flight_Chicken.png");
+    loadimage(&ENEMI_A[1][0], "images\\Flight_Chicken_SMALL.jpg");
+    loadimage(&ENEMI_A[1][1], "images\\Flight_Chicken_SMALL.png");
+
     //在(0 ,0)坐标显示图片
     putimage(0, 0, &图片);
     
     cout << "加载结束！\n";
 }
+//生成大飞机和小飞机的函数在这里！！！
+void enemyhelth(int i) {
+    if (rand()%100 < 10)
+    {
+        enemies[i].type = BIG_PLANE;
+        enemies[i].health = BIG_PLANE_DEFAULT_HP;
+        enemies[i].Width = BIG_PLANE_IMAGE_WIDTH;
+        enemies[i].Height = BIG_PLANE_IMAGE_HEIGHT;
+    }
+    else {
+        enemies[i].type = SMALL_PLANE;
+        enemies[i].health = SMALL_PLANE_DEFAULT_HP;
+        enemies[i].Width = SMALL_PLANE_IMAGE_WIDTH;
+        enemies[i].Height = SMALL_PLANE_IMAGE_HEIGHT;
+    }
 
-
+}
 void GameStartUp() {
     //创建窗口，并且设置为显示控制台
     initgraph(WIDTH, HEIGHT, EX_SHOWCONSOLE);
+
+    //初始化玩家
+    player.x = WIDTH/2.25;
+    player.y = HEIGHT-75;
 
     //初始化子弹
     for (int i = 0; i < max_bullet_num; i++)
@@ -76,17 +124,26 @@ void GameStartUp() {
         bull[i].alive = false;
         //让所有子弹状态设置为挂掉了[不存在]
     }
+
+    //初始化敌机
+    for (int i = 0; i < MAX_ENEMI_NUM; i++)
+    {
+        enemies[i].alive = false;
+        enemyhelth(i);//这行代码不仅用来设置血量，也用来设置飞机状态
+    }
 }
 
 //创建子弹
 void create_bullet() {
-    for (int i = 0; i < max_bullet_num; i++)
+    for (int i = 0; i < max_bullet_num && LastBullTick < GameTicks; i++)
     {
         if (!bull[i].alive)
         {
             bull[i].x = player.x + 34;
             bull[i].y = player.y;
             bull[i].alive = true;
+
+            LastBullTick = GameTicks + 5;
             break;
         }
 
@@ -99,11 +156,31 @@ void GameDraw() {
     putimage(player.x, player.y, &Img_role[1], NOTSRCERASE);
     putimage(player.x, player.y, &Img_role[0], SRCINVERT);
 
+    //绘制子弹
     for (int i = 0; i < max_bullet_num; i++)
     {
         if (bull[i].alive) {
             putimage(bull[i].x, bull[i].y, &Img_bullet[1], NOTSRCERASE);
             putimage(bull[i].x, bull[i].y, &Img_bullet[0], SRCINVERT);
+        }
+
+    }
+
+    //绘制敌机
+    for (int i = 0; i < MAX_ENEMI_NUM; i++)
+    {
+        if (enemies[i].alive) {
+            if (enemies[i].type==BIG_PLANE) {
+                putimage(enemies[i].x, enemies[i].y, &ENEMI_A[1][0], NOTSRCERASE);
+                putimage(enemies[i].x, enemies[i].y, &ENEMI_A[1][1], SRCINVERT);
+
+            }
+            else
+            {
+                putimage(enemies[i].x, enemies[i].y, &ENEMI_A[0][0], NOTSRCERASE);
+                putimage(enemies[i].x, enemies[i].y, &ENEMI_A[0][1], SRCINVERT);
+            }
+            
         }
 
     }
@@ -157,6 +234,19 @@ void PlayerMove(int speed) {
     }
 }
 
+void BulletMove(int speed) {
+    for (int i = 0; i < max_bullet_num; i++)
+    {
+        if (bull[i].alive&&bull[i].y>-15) {
+            bull[i].y -= speed;
+        }
+        else
+        {
+            bull[i].alive = false;
+        }
+
+    }
+}
 
 int main()
 {
@@ -172,12 +262,16 @@ int main()
     BeginBatchDraw();//启动双缓冲绘图
 
     //主进程循环(指调用循环)
-    while (114514)
+    while (true)
     {
         Sleep(10);
-        GameDraw();
+        BulletMove(Bullet_Speed);
+        
         PlayerMove(player.speed_PX);
+
+        GameDraw();
         FlushBatchDraw();//刷新绘制
+        GameTicks += 1;
     }
     EndBatchDraw();//结束双缓冲绘图
 }
